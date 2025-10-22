3138f16d7fce955550639c4829b5efa63f8eca6e
Julian Silden Langlo <langlo94@gmail.com>
2025-10-22 22:53:10 +0100

    Change loading.

--- Program.cs
+++ Program.cs
@@ -8,17 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
-
 Log.Information("Started Proxy Server");
 builder.Host.UseSerilog();
+
 builder.Services.AddCors();
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
-app.MapReverseProxy();
 app.UseStaticFiles();
+app.MapReverseProxy();
 
 app.Run();
